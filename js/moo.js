cdbd3ccda46be2d9a4172e3e7acfcfd3dc2c27bd
samtenka <samtenka@umich.edu>
2017-09-03 18:31:44 -0400

    nest

--- js/moo.js
+++ js/moo.js
@@ -1,11 +1,11 @@
 function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+    for (i = 0; i != tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
+    for (i = 0; i != tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(cityName).style.display = "block";
@@ -16,12 +16,12 @@ openCity(event, 'Home')
 
 function openCity2(evt, cityName) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i != tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
+    tablinks = document.getElementsByClassName("tablinks2");
+    for (i = 0; i != tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(cityName).style.display = "block";
