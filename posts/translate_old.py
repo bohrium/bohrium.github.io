a1d74e01727a250e26f91acb751eb31e30f94e58
samtenka <samtenka@umich.edu>
2018-05-13 19:31:04 -0400

    revise mookdown translator

--- posts/translate_old.py
+++ posts/translate_old.py
@@ -9,16 +9,6 @@
 
 from sys import argv
 
-def translate_link(source):
-    assert(source[0]=='<')
-    assert(source[-1]=='>')
-    url, text = source[1:-1].split('|')
-
-    if url.split('.')[-1] in ('jpg', 'png'):
-        return '\n<figure style="text-align:center"><img src="%s" alt="%s"></figure><figcaption style="text-align:center">%s</figcaption>' % (url, text, translate_line(text)) 
-    else:
-        return '\n<a href="%s">%s</a>' % (url, translate_line(text)) 
-
 def translate_line(line):
     ''' Convert a line of Mookdown into html'''
     style_flags = {'*':False, '`':False, '%':False, '~':False}
@@ -26,27 +16,15 @@ def translate_line(line):
     def process_special_char(c):
         style_flags[c] = not style_flags[c]
         return '<%s%s>' % ('' if style_flags[c] else '/', style_tags[c])
-
+ 
     html = ''
-
     # a. translate: 
-    i = 0
-    while i != len(line):
-        c = line[i]
-        if c=='<':
-            source = line[i:1+i+line[i:].find('>')]
-            html += translate_link(source)
-            i += len(source)-1
-        elif c in style_flags:
-            html += process_special_char(c)
-        else:
-            html += c
-        i += 1
-
+    for c in line:
+        if c in style_flags: html += process_special_char(c)
+        else: html += c
     # b. clean up unclosed styles:
     for c in style_flags:
         if style_flags[c]: html += process_special_char(c)
-
     return html
 
 def translate(text):
@@ -54,18 +32,21 @@ def translate(text):
     template = open('template.html').read()
     html = ''
     for line in text.split('\n'):
-        if not line.strip():
-            html += '\n<p>' 
-        elif line[0]=='#':
-            continue
+        if not line.strip(): html += '\n<p>' 
+        elif line[0]=='#': continue
         elif line[0]=='!':
             for i,c in enumerate(line):
                 if c!='!': break
             html += '\n<h%d> %s </h%d>' % (i+1, translate_line(line[i:]), i+1)
+        elif line[0]=='[':
+            url, text = line[1:-1].split('][')
+            if url.split('.')[-1] in ('jpg', 'png'):
+                html += '\n<figure style="text-align:center"><img src="%s" alt="%s"></figure><figcaption style="text-align:center">%s</figcaption>' % (url, text, text)
+            else:
+                html += '\n<a href="%s">%s</a>' % (url, text)
         elif line[0]=='$':
             html += '\n' + line
-        else:
-            html += '\n' + translate_line(line)
+        else: html += '\n' + translate_line(line)
     return template.replace('BODY', html)
 
 if __name__=='__main__':
