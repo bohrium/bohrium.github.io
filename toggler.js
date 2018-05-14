701de67b3e9d427ede83c743445d89e9071391a5
samtenka <samtenka@umich.edu>
2018-05-13 23:55:38 -0400

    cookie

--- toggler.js
+++ toggler.js
@@ -4,10 +4,12 @@ function setCookie(cname, cvalue) {
 
 function getCookie(cname) {
     var nameEQ = name + "=";
+    alert("##"+document.cookie);
     var ca = document.cookie.split(';');
     for (var i=0; i!=ca.length; ++i) {
         var c = ca[i];
         while (c.charAt(0) == ' ') { c = c.substring(1, c.length); }
+        alert("@"+ c);
         if (c.indexOf(nameEQ) == 0) { return c.substring(nameEQ.length, c.length); }
     }
     return null;
